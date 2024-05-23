8f497b475f26934ff261f91eddcf9444e897d08a
NickMercer <nickmercerdev@gmail.com>
2024-05-23 11:23:58 -0700

    subscription endpoint and database setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::{configuration, startup};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let config = configuration::get_configuration().expect("Failed to read configuration.");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application_port))
+        .expect("Failed to bind random port");
+    startup::run(listener)?.await
 }
