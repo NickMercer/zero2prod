4f3aa8c507aaead8d4fc90ba35c04023435f6d17
NickMercer <nickmercerdev@gmail.com>
2024-06-04 14:15:32 -0700

    chapter 5 complete

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@ async fn main() -> Result<(), std::io::Error> {
     
     let config = configuration::get_configuration().expect("Failed to read configuration.");
     
-    let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool = PgPool::connect_lazy_with(config.database.with_db());
 
-    let address = format!("127.0.0.1:{}", config.application_port);
+    let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(address)
         .expect("Failed to bind random port");
 
